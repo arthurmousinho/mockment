b6d4bfcbdea36262b1a638777c424470cc45c789
arthurmousinho <arthursilvamo296@gmail.com>
2026-08-10 15:48:26 -0300

    feat: add pagination into find all payment events endpoint

--- apps/api/src/app/routes/payment-event.routes.ts
+++ apps/api/src/app/routes/payment-event.routes.ts
@@ -1,9 +1,11 @@
 import type { FastifyInstance } from "fastify";
 import { paymentEventService } from "../services/payment-event.service.ts";
+import { paginationSchema } from "../../common/pagination.schema.ts";
 
 export async function paymentEventRoutes(app: FastifyInstance) {
   app.get("/", async (request, reply) => {
-    const events = await paymentEventService.findAll();
+    const paginationInput = paginationSchema.parse(request.query);
+    const events = await paymentEventService.findAll(paginationInput);
     return reply.status(200).send(events);
   });
 }
--- apps/api/src/app/services/payment-event.service.ts
+++ apps/api/src/app/services/payment-event.service.ts
@@ -1,6 +1,11 @@
 import { prismaSingleton } from "../../config/prisma.ts";
 import { webhookDeliveryService } from "./webhook-delivery.service.ts";
 import type { PaymentEventType } from "../../../generated/prisma/client.ts";
+import type { PaginationInput } from "../../common/pagination.schema.ts";
+import {
+  buildPaginatedResponse,
+  buildPrismaPaginationParams,
+} from "../../common/utils.ts";
 
 async function save(input: {
   type: PaymentEventType;
@@ -18,9 +23,21 @@ async function save(input: {
   return event;
 }
 
-async function findAll() {
-  return await prismaSingleton.paymentEvent.findMany({
-    orderBy: { createdAt: "desc" },
+async function findAll(paginationInput: PaginationInput) {
+  const { skip, take } = buildPrismaPaginationParams(paginationInput);
+  const [paymentEvents, total] = await Promise.all([
+    prismaSingleton.paymentEvent.findMany({
+      skip,
+      take,
+      orderBy: { createdAt: "desc" },
+    }),
+    prismaSingleton.paymentEvent.count(),
+  ]);
+  return buildPaginatedResponse({
+    data: paymentEvents,
+    page: paginationInput.page,
+    limit: paginationInput.limit,
+    total,
   });
 }
 
